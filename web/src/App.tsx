6400a52246ec8fe278cb0d5f4282609c424ea4b0
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-04 15:19:26 -0300

    web: initialize project

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proffy</h1>
+    </div>
+  );
+}
+
+export default App;
