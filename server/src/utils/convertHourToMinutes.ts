dfc56d64a796729bf368993726ae12cd757878d5
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-05 18:04:16 -0300

    server: create minutes conversion utility

---
+++ server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,6 @@
+export function convertHourToMinutes(time: string) {
+  const [hour, minutes] = time.split(':').map(Number);
+  const timeInMinutes = (hour * 60) + minutes;
+
+  return timeInMinutes;
+}
