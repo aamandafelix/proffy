e1fbd0e2af31fa56efe5db1e95bc585ccc13a4ba
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-07 18:27:06 -0300

    mobile: create reusable page header component

---
+++ mobile/src/components/PageHeader/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+    backgroundColor: '#8257E5',
+  },
+
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  title: {
+    color: '#FFF',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 24,
+    lineHeight: 32,
+    maxWidth: 160,
+    marginVertical: 40,
+  },
+});
+
+export default styles;
