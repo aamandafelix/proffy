531771d895986fd0e9d34fd1c541b8fc9ca5f7d5
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-07 18:30:35 -0300

    mobile: change tab icon focused color

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -47,9 +47,13 @@ function StudyTabs() {
         component={TeacherList}
         options={{
           tabBarLabel: 'Proffys',
-          tabBarIcon: ({ color, size }) => {
+          tabBarIcon: ({ color, size, focused }) => {
             return (
-              <Ionicons name="ios-easel" size={size} color={color} />
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? '#8257E5' : color}
+              />
             );
           }
         }}
@@ -59,9 +63,13 @@ function StudyTabs() {
         component={Favorites}
         options={{
           tabBarLabel: 'Favoritos',
-          tabBarIcon: ({ color, size }) => {
+          tabBarIcon: ({ color, size, focused }) => {
             return (
-              <Ionicons name="ios-heart" size={size} color={color} />
+              <Ionicons
+                name="ios-heart"
+                size={size}
+                color={focused ? '#8257E5' : color}
+              />
             );
           }
         }}
