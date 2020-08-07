a50c6ede95ecb81633089f91e216da52623de734
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-07 18:33:27 -0300

    mobile: add header in favorites page and teacher list page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function Favorites() {
   return (
-    <View style={styles.container} />
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
   );
 }
 
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function TeacherList() {
   return (
-    <View style={styles.container} />
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis" />
+    </View>
   );
 }
 
