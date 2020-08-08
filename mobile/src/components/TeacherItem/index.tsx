19b1b5943bfdad8146a3af2c7ceccc0121c4a135
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-07 23:20:14 -0300

    mobile: add functionality to record connection

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -3,12 +3,14 @@ import { View, Image, Text, Linking } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { RectButton } from 'react-native-gesture-handler';
 
-import styles from './styles';
+import api from '../../services/api';
 import { Teacher } from '../../types';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+
+import styles from './styles';
 
 interface TeacherItemProps {
   teacher: Teacher;
@@ -18,7 +20,15 @@ interface TeacherItemProps {
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
 
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   function handleLinkToWhatsapp() {
+    createNewConnection();
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
   }
 
